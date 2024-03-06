9ac44816f4b9893f7844b040ed1c1582bb8183a8
kesavan <kesavan338@gmail.com>
2024-03-06 17:02:59 +0530

    razo

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-import express from "express";
-import mysql from "mysql";
-import cors from "cors";
-import http from "http";
-import bodyParser from "body-parser";
-import session from "express-session";
-import cookieParser from "cookie-parser";
-import twilio from "twilio";
-import nodemailer from "nodemailer";
+const express = require("express")
+const mysql = require ("mysql")
+const cors = require ("cors")
+const http = require("http")
+const bodyParser = require("body-parser")
+const session = require("express-session")
+const cookieParser = require("cookie-parser")
+const twilio = require("twilio")
+const nodemailer = require("nodemailer")
+const dotenv = require("dotenv")
+const Razorpay = require("razorpay")
+const crypto =require ("crypto")
+dotenv.config({path:"./.env"})
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(express.urlencoded({extended:false}))
 app.use(
   session({
     secret: "secret",
@@ -28,7 +33,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: ["https://master--timely-gnome-2ae3d3.netlify.app"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST","PUT","DELETE"],
     credentials: true,
     // origin:"*"
@@ -36,16 +41,16 @@ app.use(
 );
 
 const connection = mysql.createConnection({
-  // host: "localhost",
-  // user: "root",
-  // password: "root123",
-  // database: "razotransutility",
-  // port: 3306,
-  host : "db4free.net",
-  user : "kesavan",
-  password : "Kesavan@5",
-  database : "travelixapp",
-  port : 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT,
+  // host : "db4free.net",
+  // user : "kesavan",
+  // password : "Kesavan@5",
+  // database : "travelixapp",
+  // port : 3306
 });
 
 connection.connect((error) => {
@@ -63,11 +68,106 @@ const transporter = nodemailer.createTransport({
     pass: "bmffuhcesuumgcux",
   },
 });
+//  const key_id = process.env.RAZORPAY_KEY_ID;
+// const secret = process.env.RAZORPAY_SECRET
+//  console.log(key_id,secret)
+const accountSid = process.env.accountSid
+const authToken = process.env.authToken
 
-const accountSid = "AC176601214a8c16f269337c1172e2788d";
-const authToken = "3f87ab8e009dc0f4ef805003849e86e5";
+const client = twilio(accountSid,authToken);
 
-const client = twilio(accountSid, authToken);
+app.post("/order", async(req,res )=>{
+  try{
+ const razorpay =  new Razorpay({
+     key_id: process.env.RAZORPAY_KEY_ID,
+     key_secret: process.env.RAZORPAY_SECRET
+    })
+   
+    const options = {
+        amount:req.body.amount,
+        currency:req.body.currency,
+        receipt:req.body.receiptID
+    };
+    console.log(options)
+    const order = await razorpay.orders.create(options)
+
+    if (!order){
+      return res.status(500).send("error")
+    }
+    else{
+      res.status(200).send(order);
+  //    const data = req.body;
+  //    console.log(data);
+
+  //    const sqlQuery = `insert into mobilerecharge (circle,operator,mobileNumber,rechargeAmount,tpin,rechargeTime,transactionId) values ('${data.circle}','${data.operator}','${data.mobileNumber}','${data.amount}','${data.tpin}',now(),'${data.receiptID}')`;
+  //    connection.query(sqlQuery, (error, results) => {
+  //   if (error) {
+  //     res.status(500).json({ message: "Internal server error" });
+  //     return;
+  //   } else {
+  //     res.status(200).json(results);
+  //   }
+
+    
+  // });
+
+    }
+    
+
+  
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).send("error")
+  }
+
+   
+})
+
+app.post("/order/validate",(req,res)=>{
+  const {razorpay_order_id,razorpay_payment_id,razorpay_signature} = req.body;
+  const sha = crypto.createHmac("sha256",process.env.RAZORPAY_SECRET);
+  sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+  const digest = sha.digest("hex");
+  if(digest !== razorpay_signature){
+    return res.status(400).send({msg:"Transaction is not legit"})
+  }
+    res.json({
+    msg:"success",
+    orderId:razorpay_order_id,
+    paymentId:razorpay_payment_id
+  })
+
+  // const mailOptions = {
+  //   from: "youremail@gmail.com",
+  //   to: "kesavanraj338@gmail.com",
+  //   subject: `Recharged`,
+  //   text: "your request payment successfully recharged",
+  // };
+
+  // transporter.sendMail(mailOptions, function (error, info) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log("Email sent: " + info.response);
+  //   }
+  // });
+  // const amount = `${data.amount}`;
+  // const fixedAmount = (amount * 25.3) / 100;
+  // const reducedAmount = (amount - fixedAmount).toFixed(2);
+  // console.log(reducedAmount);
+
+  // client.messages
+  //   .create({
+  //     body: `Recharge done on now() MRP:INR ${data.amount}.00.GST 18% payabel by comapny/Distributor/retailer,Talktime:RS ${reducedAmount},Bal:INR,TransID ${transactionId}.Check your balance,validity,tariff and best recharges on ${data.operator} Thanks App.Would you like to refer Airtel Prepaid to your friends & family and save Rs 100 on your next`,
+  //     to: "+918525905774", // Text your number
+  //     from: "+12512205586", // From a valid Twilio number
+  //   })
+  //   .then((message) => console.log(message.sid))
+  //   .catch((err) => console.log(err));
+
+  
+})
 
 app.post("/login", (req, res) => {
   const data = req.body;
@@ -104,36 +204,9 @@ app.post("/mobile/recharge", (req, res) => {
       return;
     } else {
       res.status(200).json(results);
-
-      const amount = `${data.rechargeAmount}`;
-      const fixedAmount = (amount * 25.3) / 100;
-      const reducedAmount = (amount - fixedAmount).toFixed(2);
-      console.log(reducedAmount);
-
-      client.messages
-        .create({
-          body: `Recharge done on now() MRP:INR ${data.rechargeAmount}.00.GST 18% payabel by comapny/Distributor/retailer,Talktime:RS ${reducedAmount},Bal:INR,TransID ${transactionId}.Check your balance,validity,tariff and best recharges on ${data.operator} Thanks App.Would you like to refer Airtel Prepaid to your friends & family and save Rs 100 on your next`,
-          to: `+91'${data.mobileNumber}'`, // Text your number
-          from: "+12512205586", // From a valid Twilio number
-        })
-        .then((message) => console.log(message.sid))
-        .catch((err) => console.log(err));
     }
 
-    const mailOptions = {
-      from: "youremail@gmail.com",
-      to: "kesavanraj338@gmail.com",
-      subject: `Recharged`,
-      text: "your request payment successfully recharged",
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
+    
   });
 });
 
@@ -201,7 +274,7 @@ app.post("/addMobile", (request, response) => {
 });
 
 app.get("/list/mobile/recharge", (req, res) => {
-  const sqlQuery = `select * from mobilerecharge`;
+  const sqlQuery = `select * from mobilerecharge order by id DESC`;
   connection.query(sqlQuery, (error, results) => {
     if (error) {
       res.status(500).send({
